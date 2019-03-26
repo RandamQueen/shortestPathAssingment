9913c2175e99c007de911fef02b5bcce01fae696
Hannah Keating <34624322+RandamQueen@users.noreply.github.com>
2019-03-26 10:32:00 +0000

    Update CompetitionTests.java

--- src/CompetitionTests.java
+++ src/CompetitionTests.java
@@ -1,4 +1,18 @@
+import org.junit.jupiter.api.Test;
 
 public class CompetitionTests {
 
+    @Test
+    public void testDijkstraConstructor() {
+
+        //TODO
+    }
+
+    @Test
+    public void testFWConstructor() {
+        //TODO
+    }
+
+    //TODO - more tests
+    
 }
